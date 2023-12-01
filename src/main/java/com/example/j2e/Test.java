a95ce17002c88fcf64920dab35c2f1883cdff763
Lorenzo <lorenzo-a@live.fr>
2023-12-01 17:28:33 +0100

    Add jsp

--- src/main/java/com/example/j2e/Test.java
+++ src/main/java/com/example/j2e/Test.java
@@ -7,8 +7,9 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name = "Test", value = "/Test")
+@WebServlet(name = "Test", value = "/test")
 public class Test extends HttpServlet {
 
     private static final long serialVersionUID = 1L;
@@ -19,13 +20,11 @@ public class Test extends HttpServlet {
 
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Logique de traitement pour les requêtes HTTP GET
-        response.getWriter().append("Réponse du servlet : ").append(request.getContextPath());
+        this.getServletContext().getRequestDispatcher("/WEB-INF/bonjour.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        // Logique de traitement pour les requêtes HTTP POST
         doGet(request, response);
     }
 }
---
+++ src/main/webapp/WEB-INF/bonjour.jsp
@@ -0,0 +1,17 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: lorenzo
+  Date: 01/12/2023
+  Time: 17:10
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+    <title>Title</title>
+</head>
+<body>
+<h1>Coucou</h1>
+
+</body>
+</html>
